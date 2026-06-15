cecbaade27862ea868e420aee7046408c3a735a2
Виктор <kasa.king_over@mail.ru>
2026-06-15 15:30:00 +0300

    feat(domain): add persistence column constants

---
+++ src/fitnes.Domain/Constants/Persistence/NutritionJsonConstants.cs
@@ -0,0 +1,11 @@
+namespace fitnes.Domain.Constants.Persistence;
+
+public static class NutritionJsonConstants
+{
+    public const string CaloriesKcal = "calories_kcal";
+    public const string ProteinG = "protein_g";
+    public const string FatG = "fat_g";
+    public const string CarbsG = "carbs_g";
+    public const string Min = "min";
+    public const string Max = "max";
+}
